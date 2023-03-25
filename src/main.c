86fcea3325ea26ae6ce3590719e259efbf168a23
wwotz <lukeanthonyholland@gmail.com>
2023-03-25 19:01:05 +0000

    updated main.c for testing.

--- src/main.c
+++ src/main.c
@@ -9,14 +9,34 @@ int main(int argc, char **argv)
                 exit(EXIT_FAILURE);
         }
 
+        tt_program_info_t rect_pinfo = {
+                { TT_SHADER_FILE_TYPE, GL_VERTEX_SHADER, "../shaders/rect.vert" },
+                { TT_SHADER_FILE_TYPE, GL_FRAGMENT_SHADER, "../shaders/rect.frag" }
+        };
+        GLuint rect_program = tt_program_create(rect_pinfo);
+        if (tt_debug_had_error()) {
+                fprintf(stderr, "%s\n", tt_debug_stack_pop());
+                exit(EXIT_FAILURE);
+        }
+
+        tt_rect_t rect;
+        tt_rect_init(&rect, 0.0, 0.0, 50.0, 50.0,
+                     rect_program, 0, 0xff0000ff);
+        if (tt_debug_had_error()) {
+                fprintf(stderr, "%s\n", tt_debug_stack_pop());
+                exit(EXIT_FAILURE);
+        }
+
         while (tt_window_is_running()) {
                 while (tt_window_poll_event()) {
                         tt_window_events();
                 }
                 tt_window_clear_buffers(GL_COLOR_BUFFER_BIT);
+                tt_rect_render(&rect);
                 tt_window_swap();
         }
 
+        tt_rect_free(&rect);
         tt_window_free();
         return 0;
 }

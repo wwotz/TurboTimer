0429e607e627dd164cf296e729357ebbb7acf5d9
wwotz <lukeanthonyholland@gmail.com>
2023-03-25 17:30:05 +0000

    added utils.c for file mapping.

---
+++ src/utils.c
@@ -0,0 +1,41 @@
+#include "../include/tt.h"
+
+size_t tt_utils_fsize(FILE *fd)
+{
+        if (!fd) return 0;
+        size_t fsize;
+        fseek(fd, 0, SEEK_END);
+        fsize = ftell(fd);
+        fseek(fd, 0, SEEK_SET);
+        return fsize;
+}
+
+char *tt_utils_fmap(const char *path)
+{
+        char *buffer;
+        size_t buffer_len;
+        FILE *fd;
+
+        fd = fopen(path, "r");
+        if (!fd) {
+                TT_PUSH_ERROR("File does not exist!");
+                return NULL;
+        }
+
+        buffer_len = tt_utils_fsize(fd);
+        buffer = malloc(buffer_len + 1);
+
+        size_t nread;
+        while ((nread = fread(buffer, sizeof(*buffer), buffer_len, fd)) > 0)
+                ;
+
+        if (feof(fd)) {
+                buffer[buffer_len] = '\0';
+        } else if (ferror(fd)) {
+                free(buffer);
+                buffer = NULL;
+        }
+
+        fclose(fd);
+        return buffer;
+}
